3a9560570993d91f5de839c774b15e4e7187b4fb
aruzikulov <ruzikulov@gmail.com>
2018-06-02 20:08:23 +0800

    second commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database: 'mongodb://localhost:27017/meanauthapp',
+  //'mongodb://<username>:<password>@ds12226.mlab.com:12226/meanauthapp',   //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: 'yoursecret'
+}
